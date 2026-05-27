f1de2c197a763546d74a9baa7e33e9c451fb505a
zach-all <zacha.allen96@gmail.com>
2026-05-27 07:54:40 +0000

    Update dashboard-artifact to output generated at 1b2354c

---
+++ _next/static/OC0QX-qlfly6U4XhWfhLc/_buildManifest.js
@@ -0,0 +1,90 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/0iz8gqf5fu0d_.js"
+  ],
+  "/_error": [
+    "static/chunks/0dk_crv70-idb.js"
+  ],
+  "/await_tree": [
+    "static/chunks/0e_sc22en6wdv.js"
+  ],
+  "/batch_tasks": [
+    "static/chunks/10bdqq-vop5xz.js"
+  ],
+  "/cluster": [
+    "static/chunks/0i7c7y13lhx4u.js"
+  ],
+  "/explain_distsql": [
+    "static/chunks/0c7fncmnnnh2p.js"
+  ],
+  "/fragment_graph": [
+    "static/chunks/13t~4m~ny63jq.js"
+  ],
+  "/functions": [
+    "static/chunks/0knx24f9ldw4h.js"
+  ],
+  "/heap_profiling": [
+    "static/chunks/0g8nxkzo~rde7.js"
+  ],
+  "/indexes": [
+    "static/chunks/08zr4z380m27o.js"
+  ],
+  "/internal_tables": [
+    "static/chunks/17b~ro.fkf96q.js"
+  ],
+  "/materialized_views": [
+    "static/chunks/13m1klcnnuqub.js"
+  ],
+  "/node": [
+    "static/chunks/0rgkgjyrmwz9z.js"
+  ],
+  "/relation_graph": [
+    "static/chunks/034c_kvrj67we.js"
+  ],
+  "/settings": [
+    "static/chunks/18amoa7z9h9qw.js"
+  ],
+  "/sinks": [
+    "static/chunks/0oib2yihsmf96.js"
+  ],
+  "/sources": [
+    "static/chunks/0j5qs-v873g.r.js"
+  ],
+  "/subscriptions": [
+    "static/chunks/0kix87jdq7xda.js"
+  ],
+  "/tables": [
+    "static/chunks/07r6utx5jh6rx.js"
+  ],
+  "/views": [
+    "static/chunks/117s.iimaoy8m.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/_app",
+    "/_error",
+    "/await_tree",
+    "/batch_tasks",
+    "/cluster",
+    "/explain_distsql",
+    "/fragment_graph",
+    "/functions",
+    "/heap_profiling",
+    "/indexes",
+    "/internal_tables",
+    "/materialized_views",
+    "/node",
+    "/relation_graph",
+    "/settings",
+    "/sinks",
+    "/sources",
+    "/subscriptions",
+    "/tables",
+    "/views"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
